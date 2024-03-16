39946281cd8c778c09b8d508e7a2f1df0640fa89
Tarasov Nikita <nvisary@gmail.com>
2024-03-16 23:12:23 +0300

    rocket physics update

--- rocket.gd
+++ rocket.gd
@@ -1,34 +1,48 @@
 extends RigidBody2D
 
-var yforce = 50000 
-var xforce = 25000
-var torque = 1000000
+var force_multipler = 1000000
 
-@onready var particle_left: CPUParticles2D = $Left
-@onready var particle_right: CPUParticles2D = $Right
+@export var SHOW_DEBUG = true
+
+var yforce = 50 * force_multipler 
+var xforce = 25 * force_multipler
+var torque = 2 * force_multipler
+
+var max_velocity = Vector2(300, 200)
+
+@onready var vspeed_label = $VSpeedLabel
+@onready var hspeed_label = $HSpeedLabel
 
 func _ready():
+	if SHOW_DEBUG:
+		vspeed_label.show()
+		hspeed_label.show()
+	
 	set_process(true)
+	
+func display_speed():
+	vspeed_label.text = 'v.speed: ' + str(round(linear_velocity.y))
+	hspeed_label.text = 'h.speed: ' + str(round(linear_velocity.x))
+	
+func controls(delta):
+	if Input.is_action_pressed("left_help_engine") and linear_velocity.x > -300:
+		var force = Vector2(-xforce * delta, 0)
+		apply_central_force(force.rotated(rotation))
+	if Input.is_action_pressed("right_help_engine") and linear_velocity.x < 300:
+		var force = Vector2(xforce * delta, 0)
+		apply_central_force(force.rotated(rotation))
+	if Input.is_action_pressed('main_engine'):
+		var force = Vector2(0, -yforce) * delta
+		apply_central_force(force.rotated(rotation))
+		
+func _integrate_forces(state):
+	if rotation < -0.01 and Input.is_action_pressed("main_engine"):
+		apply_torque(torque)
+	if rotation > 0.01 and Input.is_action_pressed("main_engine"):
+		apply_torque(-torque)
 
 func _process(delta):
-	if Input.is_action_just_released("left_engine"):
-		particle_left.hide()
-		
-	if Input.is_action_just_released("right_engine"):
-		particle_right.hide()
-	
-	if Input.is_action_pressed('rotation_left'):
-		apply_torque(-torque * delta)
-	if Input.is_action_pressed('rotation_right'):
-		apply_torque(torque * delta)
-	if Input.is_action_pressed('left_engine') and not Input.is_action_pressed('right_engine'):
-		particle_left.show()
-		apply_central_force(Vector2(xforce * delta, 0).rotated(rotation))
-	if Input.is_action_pressed('right_engine') and not Input.is_action_pressed("left_engine"):
-		particle_right.show()
-		apply_central_force(Vector2(-xforce * delta, 0).rotated(rotation))
-	if Input.is_action_pressed('left_engine') and Input.is_action_pressed('right_engine'):
-		particle_right.show()
-		particle_left.show()	
-		apply_central_force(Vector2(0,-yforce * delta).rotated(rotation))
+	controls(delta)
+	if SHOW_DEBUG:
+		display_speed()
 		
